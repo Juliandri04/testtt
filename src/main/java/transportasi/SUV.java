3ab0698dac26d35d64eae1c9854b49b03583d4ea
Juliandri04 <94310353+Juliandri04@users.noreply.github.com>
2024-06-12 21:42:52 +0700

    Update SUV.java

--- src/main/java/transportasi/SUV.java
+++ src/main/java/transportasi/SUV.java
@@ -1,33 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package transportasi;
+import transportasi.*;
 
-/**
- *
- * @author asus
- */
-public class SUV extends Car{
+public class SUV extends Kendaraan {
     private int groundClearence;
-    
+
     public SUV(int xLoc, int yLoc, String plateNum, double speed) {
         super(xLoc, yLoc, plateNum, speed);
+        this.groundClearence = 0;
     }
 
     public int getGroundClearence() {
         return groundClearence;
     }
 
-    public void setGroundClearence(int groundClearence) {
-        this.groundClearence = groundClearence;
-    }
-
     @Override
     public String toString() {
-        return "SUV{" + "groundClearence=" + groundClearence + '}';
+        return "SUV{" +
+                "groundClearence=" + groundClearence +
+                ", xLoc=" + getXLoc() +
+                ", yLoc=" + getYLoc() +
+                ", plateNum='" + getPlateNum() + '\'' +
+                ", speed=" + getSpeed() +
+                '}';
     }
-    
-    
-    
-};
+}
